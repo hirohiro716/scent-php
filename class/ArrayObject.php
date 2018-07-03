dfe71b4cde7d758a3bcfae8d85c63141e5247d34
hiro <hiro@nova>
2018-07-03 15:47:58 +0900

    Creating class for ArrayObject.

---
+++ class/ArrayObject.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * 連想配列オブジェクトクラス.
+ * @author hiro
+ */
+class ArrayObject {
+    
+    /**
+     * コンストラクタ.
+     * @param array $array
+     */
+    public function __construct(array $array) {
+        $this->array = $array;
+    }
+    
+    /**
+     * コンストラクタ.
+     */
+    public function __construct() {
+        $this(Array());
+    }
+    
+    private $array;
+    
+    /**
+     * 内部に配列をセットする.
+     * @param array $array
+     */
+    public function setArray(array $array) {
+        $this->array = $array;
+    }
+    
+    /**
+     * 内部の配列を取得する.
+     * @return array
+     */
+    public function getArray() {
+        return $this->array;
+    }
+    
+    /**
+     * キーと対する値をセットする.
+     * @param string $key
+     * @param object $value
+     */
+    public function put(string $key, object $value) {
+        $this->array[$key] = $value;
+    }
+    
+    /**
+     * キーに対する値を取得する.
+     * @param string $key
+     * @return object
+     */
+    public function get(string $key) {
+        return $this->array[$key];
+    }
+    
+}
